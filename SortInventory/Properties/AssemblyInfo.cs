d5e9e3fdeb03659bea46aea1e46d06b5576c9706
mcglonelevi <mcglonelevi@yahoo.com>
2019-01-14 20:38:42 -0500

    first commit

---
+++ SortInventory/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyTitle("SortInventory Mod")]
+[assembly: AssemblyDescription("This assembly is a Mod for RaftModLoader wich can be downloaded at https://www.raftmodding.com")]
+[assembly: AssemblyCompany("HyTeKGames")]
+[assembly: AssemblyProduct("RaftModLoader Mod")]
+[assembly: AssemblyCopyright("Copyright © HyTeK Games")]
+[assembly: AssemblyTrademark("Copyright © HyTeK Games")]
