8ecdad960a084c960e44715da439a9032f391d61
traxam <traxam@trax.am>
2020-08-18 20:54:49 +0200

    Update mod to .rmod format and add icon+banner.

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
-# SortInventory - Sort your inventory!
+![banner image](./SortInventory/banner.png)
 
-## Description
+# SortInventory - Sort your inventory!
+*([view on RaftModding](https://www.raftmodding.com/mods/sortinventory))*
+
 This is a mod for Raft, a Unity-based game on Steam.  Open your inventory and press 'Z' to have your your inventory (minus hotbar) and any chests you have open to be automatically compressed sorted.
 
 ## Instructions
-1. Get the Raft modloader if you have not already: https://www.raftmodding.com/
-2. Grab the latest release from the releases tab.  Download the DLL and put it in your mods folder.
+1. Get the Raft Mod Loader if you have not already: [https://www.raftmodding.com/download](https://www.raftmodding.com/download)
+2. Click the green `Install mod` button on the right side of this mod's page. This will open the mod installer on your computer.
+
+## Demo
+Before sorting:
+
+![messy inventory](https://github.com/mcglonelevi/RaftModSortInventory/raw/master/messy.PNG)
+
+After sorting:
+![clean inventory](https://github.com/mcglonelevi/RaftModSortInventory/raw/master/clean.PNG)
 
 ## Contributing
-If you discover a bug or want to make an enhancement, please create an issue and/or PR.
+If you discover a bug or want to make an enhancement, please create an issue and/or PR in the [GitHub repo](https://github.com/mcglonelevi/RaftModSortInventory).
+
+## Credits
+- idea and original code by [wazupwiop](https://github.com/mcglonelevi), the original source files are available [on GitHub](https://github.com/mcglonelevi/RaftModSortInventory)
+- new implementation by [traxam](https://trax.am/)
+- icon image based on [an icon image](https://www.flaticon.com/free-icon/sort_178417) by [Freepik](https://www.freepik.com/), licensed [Creative Commons BY 3.0](http://creativecommons.org/licenses/by/3.0/)
---
+++ SortInventory/SortInventoryMod.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SortInventory
+{
+    public class SortInventoryMod : Mod
+    {
+        private void Start()
+        {
+            Debug.Log("SortInventory loaded!");
+        }
+
+        public void Update()
+        {   
+            if (CanvasHelper.ActiveMenu == MenuType.Inventory && Input.GetKeyDown(KeyCode.Z)) {
+                Network_Player player = RAPI.GetLocalPlayer();
+                if (player != null) {
+                    SortPlayerInventory(player);
+                }
+            }
+        }
+
+        private void SortPlayerInventory(Network_Player player) {
+            PlayerInventory inv = player.Inventory;
+        
+            // Get all of the inventory slots that are not in the hotbar.
+            List<Slot> nonHotbar = inv.allSlots.GetRange(player.Inventory.hotslotCount, player.Inventory.allSlots.Count - player.Inventory.hotslotCount);
+
+            Sort(nonHotbar);
+
+            if (inv.secondInventory != null)
+            {
+                Sort(inv.secondInventory.allSlots);
+            }
+        }
+
+        private void Sort(List<Slot> nonHotbar)
+        {
+            // Get a temporary list of all the used slots in the inventory.
+            List<ItemInstance> tempInstances = new List<ItemInstance>();
+
+            foreach (Slot slot in nonHotbar)
+            {
+                // Grab its amount.
+                if (!slot.IsEmpty)
+                {
+                    tempInstances.Add(slot.itemInstance.Clone());
+                }
+                // Reset the slot.
+                slot.Reset();
+            }
+
+            tempInstances.Sort((x, y) => String.Compare(x.UniqueName, y.UniqueName, StringComparison.Ordinal));
+
+            // Do some assignments to sort it in the inventory
+            var i = 0;
+            foreach (ItemInstance instance in tempInstances)
+            {
+                Slot currSlot = nonHotbar[i];
+                currSlot.SetItem(instance);
+                i++;
+            }
+
+            // Now we compress with i starting at 1
+            for (i = 1; i < nonHotbar.Count; i++)
+            {
+                Slot currSlot = nonHotbar[i];
+                if (!currSlot.IsEmpty)
+                {
+                    Slot toSlot = this.FindSuitableSlot(nonHotbar, currSlot.GetItemBase());
+                    if (toSlot != currSlot && toSlot != null)
+                    {
+                        this.TransferItems(toSlot, currSlot);
+                    }
+                }
+            }
+        }
+
+        private void TransferItems(Slot toSlot, Slot fromSlot) {
+            if (!fromSlot.itemInstance.settings_Inventory.Stackable || toSlot.IsEmpty)
+            {
+                toSlot.SetItem(fromSlot.itemInstance);
+                fromSlot.Reset();
+            }
+            else
+            {
+                int maxAmount = fromSlot.itemInstance.settings_Inventory.StackSize;
+                if (fromSlot.itemInstance.Amount + toSlot.itemInstance.Amount > maxAmount) {
+                    int diff = maxAmount - toSlot.itemInstance.Amount;
+                    fromSlot.itemInstance.Amount -= diff;
+                    toSlot.itemInstance.Amount += diff;
+                    fromSlot.RefreshComponents();
+                    toSlot.RefreshComponents();
+                } else {
+                    toSlot.itemInstance.Amount += fromSlot.itemInstance.Amount;
+                    toSlot.RefreshComponents();
+                    fromSlot.Reset();
+                }
+            }
+        }
+
+        private Slot FindSuitableSlot(List<Slot> inv, Item_Base stackableItem = null)
+        {
+            bool flag = stackableItem != null && stackableItem.settings_Inventory.Stackable;
+            foreach (Slot slot2 in inv)
+            {
+                if (slot2.IsEmpty)
+                {
+                    return slot2;
+                }
+                if (flag && !slot2.StackIsFull() && !slot2.IsEmpty && slot2.itemInstance.UniqueIndex == stackableItem.UniqueIndex)
+                {
+                    return slot2;
+                }
+            }
+            return null;
+        }
+
+        public void OnModUnload()
+        {
+            Debug.Log("SortInventory has been unloaded!");
+        }
+    }
+}
