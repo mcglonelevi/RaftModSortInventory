997951b9aca318de1f40fc5cd62bf5007ad4ea51
Daniel Sobral <dcsobral@gmail.com>
2021-01-07 15:46:20 -0700

    Update to current mod loader/raft version

--- build.bat
+++ build.bat
@@ -4,14 +4,17 @@
 @echo off
 :: Defining the window title
 title RML Mod Build Script
+:: Retrieving the current folder name
+for %%* in (.) do set foldername=%%~n*
 :: Creating a folder to contain temporary files for the build
 mkdir "build"
 :: Copying the solution directory in the "build" folder except ".csproj, .rmod" files and "bin, obj" folders.
-robocopy "SortInventory" "build" /E /XF *.csproj *.rmod /XD bin obj
+robocopy "%foldername%" "build" /E /XF *.csproj *.rmod /XD bin obj
 :: Checking if a .rmod with the same name already exists and if it does, delete it.
-if exist "SortInventory.rmod" ( del "SortInventory.rmod" )
+if exist "%foldername%.rmod" ( del "%foldername%.rmod" )
 :: Zipping the "build" folder. (.rmod are just zipped files)
-powershell "[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem');[System.IO.Compression.ZipFile]::CreateFromDirectory(\"build\", \"SortInventory.rmod\", 0, 0)"
+powershell "[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem');[System.IO.Compression.ZipFile]::CreateFromDirectory(\"build\", \"%foldername%.rmod\", 0, 0)"
 :: Deleting the "build" folder
 rmdir /s /q "build"
 :: Build succeeded!
+EXIT
